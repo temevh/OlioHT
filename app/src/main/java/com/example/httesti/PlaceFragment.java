7b8bf8682fc8b2c24c95ef2fc7ad291c4ece7223
temevh <temehama@gmail.com>
2022-04-26 17:30:35 +0300

    Paikkanäkymä backbutton aloitettu

--- app/src/main/java/com/example/httesti/PlaceFragment.java
+++ app/src/main/java/com/example/httesti/PlaceFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,13 +15,15 @@ import java.util.ArrayList;
 
 public class PlaceFragment extends Fragment {
 
+    View view;
+
     ArrayList placeInfo = new ArrayList();
     String placeName = "N/A";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.place_layout, container,false);
+        view = inflater.inflate(R.layout.place_layout, container,false);
         placeInfo = getArguments().getParcelableArrayList("placeinfo");
         placeName = getArguments().getString("placename");
 
@@ -55,4 +58,11 @@ public class PlaceFragment extends Fragment {
 
         super.onViewCreated(view, savedInstanceState);
     }
+
+
+    public void backToMainMenu(){
+        System.out.println("TOIMII");
+
+    }
+
 }
