a05803c1f06a67f2a492ea62b9af2b94e001348d
temevh <temehama@gmail.com>
2022-04-29 11:14:41 +0300

    Lisätty latausruutu käynnistykseen.

--- app/src/main/java/com/example/httesti/MainActivity.java
+++ app/src/main/java/com/example/httesti/MainActivity.java
@@ -8,9 +8,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
